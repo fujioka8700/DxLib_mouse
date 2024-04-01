cfc351677af40d3bc62a3eff5e89b7c4b1096274
Masahiro Fujioka <fujioka8700@gmail.com>
2024-04-01 19:35:40 +0900

    プロジェクト ファイルを追加します。

---
+++ source.cpp
@@ -0,0 +1,22 @@
+#include <Dxlib.h>
+
+int WINAPI WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPSTR lpCmdLine,
+	_In_ int nShowCmd)
+{
+#ifndef _DEBUG
+	SetOutApplicationLogValidFlag(FALSE);
+#endif // !_DEBUG
+
+	ChangeWindowMode(TRUE);
+
+	if (DxLib_Init() == -1) return -1;
+
+	printfDx("Hello World\n");
+	WaitKey();
+
+	DxLib_End();
+	return 0;
+}
