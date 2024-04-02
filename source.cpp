aa04ddbec705df13230cd5fa2f2c69f159547397
Masahiro Fujioka <fujioka8700@gmail.com>
2024-04-02 09:27:31 +0900

    [update] マウスカーソルの軌跡。

--- source.cpp
+++ source.cpp
@@ -1,5 +1,38 @@
-#include <Dxlib.h>
+//************************************************
+// DXライブラリ マウスでお絵かき
+// 作成日：2024/04/02
+// 作成者：fujioka8700
+// Copyright (c) fujioka8700 All rights reserved.
+//************************************************
 
+#include <Dxlib.h>
+#include "source.hpp"
+
+//================================================
+// FPSの計測と描画
+//================================================
+void FpsDraw(LONGLONG* p1, Size* p2)
+{
+#ifdef _DEBUG
+	static int Fps = 0, FpsCnt = 0;
+
+	LONGLONG now = GetNowHiPerformanceCount();
+	
+	FpsCnt++;
+	if (now - *p1 > 1000000)
+	{
+		Fps = FpsCnt;
+		FpsCnt = 0;
+		*p1 = now;
+	}
+	
+	DrawFormatString(0, p2->height - 15, GetColor(255, 255, 255), "FPS: %d", Fps);
+#endif // !_DEBUG
+}
+
+//================================================
+// 主処理
+//================================================
 int WINAPI WinMain(
 	_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
@@ -14,8 +47,23 @@ int WINAPI WinMain(
 
 	if (DxLib_Init() == -1) return -1;
 
-	printfDx("Hello World\n");
-	WaitKey();
+	LONGLONG fpsTimer, deltaTimer;
+	Size windowSize;
+	int x, y;
+
+	GetWindowSize(&windowSize.width, &windowSize.height);
+
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	while (ProcessMessage() == 0)
+	{
+		GetMousePoint(&x, &y);
+		
+		DrawCircle(x, y, 2, GetColor(255, 0, 0), TRUE);
+
+		FpsDraw(&fpsTimer, &windowSize);
+		ScreenFlip();
+	}
 
 	DxLib_End();
 	return 0;
---
+++ source.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef SOURCE_HPP_
+#define SOURCE_HPP_
+
+//================================================
+// ウィンドウのサイズ
+//================================================
+struct Size {
+	int width, height;
+};
+
+#endif // SOURCE_HPP_
